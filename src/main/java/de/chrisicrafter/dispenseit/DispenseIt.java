94cf511c1c9e2413722da3cb13f6b548541e16d6
BuildTools <unconfigured@null.spigotmc.org>
2023-10-20 23:10:18 +0200

    Release 1.0

--- src/main/java/de/chrisicrafter/dispenseit/DispenseIt.java
+++ src/main/java/de/chrisicrafter/dispenseit/DispenseIt.java
@@ -1,125 +1,147 @@
 package de.chrisicrafter.dispenseit;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.dispenser.BlockSource;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(DispenseIt.MODID)
+import java.util.List;
+
+@Mod(DispenseIt.MOD_ID)
 public class DispenseIt {
-
-    // Define mod id in a common place for everything to reference
-    public static final String MODID = "dispenseit";
-    // Directly reference a slf4j logger
+    public static final String MOD_ID = "dispenseit";
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "dispenseit" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "dispenseit" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "dispenseit:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "dispenseit:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEat().nutrition(1).saturationMod(2f).build())));
-
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-            output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
 
     public DispenseIt() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("COMMON SETUP START");
 
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        //Register block-place dispenser-behaviour
+        for(Block block : ForgeRegistries.BLOCKS.getValues()) {
+            if(block.defaultBlockState().getPistonPushReaction() == PushReaction.NORMAL || block.defaultBlockState().getPistonPushReaction() == PushReaction.PUSH_ONLY) {
+                Item item = block.asItem();
+                DispenserBlock.registerBehavior(item, new DefaultDispenseItemBehavior() {
+                    @Override
+                    protected ItemStack execute(BlockSource blockSource, ItemStack itemStack) {
+                        ServerLevel world = blockSource.level();
+                        Direction direction = blockSource.state().getValue(DispenserBlock.FACING);
+                        BlockPos pos = blockSource.pos();
+                        switch (direction) {
+                            case DOWN -> pos = pos.below();
+                            case UP -> pos = pos.above();
+                            case EAST -> pos = pos.east();
+                            case WEST -> pos = pos.west();
+                            case NORTH -> pos = pos.north();
+                            case SOUTH -> pos = pos.south();
+                        }
+                        if (world.getBlockState(pos).isAir()) {
+                            world.setBlock(pos, block.defaultBlockState(), 3);
+                            itemStack.split(1);
+                            return itemStack;
+                        }
+                        return super.execute(blockSource, itemStack);
+                    }
+                });
+            }
         }
+
+        //Register block-mine dispenser-behaviour
+        for(Item item : ForgeRegistries.ITEMS.getValues()) {
+            if(item instanceof DiggerItem) {
+                DispenserBlock.registerBehavior(item, new DefaultDispenseItemBehavior() {
+                    @Override
+                    protected ItemStack execute(BlockSource blockSource, ItemStack itemStack) {
+                        ServerLevel world = blockSource.level();
+                        Direction direction = blockSource.state().getValue(DispenserBlock.FACING);
+                        BlockPos pos = blockSource.pos();
+                        switch (direction) {
+                            case DOWN -> pos = pos.below();
+                            case UP -> pos = pos.above();
+                            case EAST -> pos = pos.east();
+                            case WEST -> pos = pos.west();
+                            case NORTH -> pos = pos.north();
+                            case SOUTH -> pos = pos.south();
+                        }
+                        BlockState blockState = world.getBlockState(pos);
+                        if (itemStack.getItem() instanceof TieredItem tieredItem
+                                && ((blockState.is(BlockTags.MINEABLE_WITH_AXE) && itemStack.getItem() instanceof AxeItem)
+                                || (blockState.is(BlockTags.MINEABLE_WITH_PICKAXE) && itemStack.getItem() instanceof PickaxeItem)
+                                || (blockState.is(BlockTags.MINEABLE_WITH_HOE) && itemStack.getItem() instanceof HoeItem)
+                                || (blockState.is(BlockTags.MINEABLE_WITH_SHOVEL) && itemStack.getItem() instanceof ShovelItem))
+                                && ((blockState.is(BlockTags.NEEDS_STONE_TOOL) && tieredItem.getTier().getLevel() >= Tiers.STONE.getLevel())
+                                || (blockState.is(BlockTags.NEEDS_IRON_TOOL) && tieredItem.getTier().getLevel() >= Tiers.IRON.getLevel())
+                                || (blockState.is(BlockTags.NEEDS_DIAMOND_TOOL) && tieredItem.getTier().getLevel() >= Tiers.DIAMOND.getLevel())
+                                || (!blockState.is(BlockTags.NEEDS_STONE_TOOL) && !blockState.is(BlockTags.NEEDS_IRON_TOOL) && !blockState.is(BlockTags.NEEDS_DIAMOND_TOOL)))
+                                && !Block.getDrops(blockState, world, pos, null, null, itemStack).isEmpty()) {
+                            List<ItemStack> drops = Block.getDrops(blockState, world, pos, null, null, itemStack);
+                            world.destroyBlock(pos, false);
+                            for (ItemStack stack : drops) {
+                                if (stack != null && stack.getCount() > 0) {
+                                    ItemEntity entityItem = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack.copy());
+                                    if (stack.hasTag()) {
+                                        entityItem.getItem().setTag(stack.getTag().copy());
+                                    }
+                                    world.addFreshEntity(entityItem);
+                                }
+                            }
+                            if (itemStack.isDamageableItem())
+                                itemStack.setDamageValue(itemStack.getDamageValue() + 1);
+                            return itemStack;
+                        }
+                        return super.execute(blockSource, itemStack);
+                    }
+                });
+            }
+        }
+
+        //Register lava-cauldron-emptying dispenser-behaviour
+        DispenserBlock.registerBehavior(Items.BUCKET, new DefaultDispenseItemBehavior() {
+            @Override
+            protected ItemStack execute(BlockSource blockSource, ItemStack itemStack) {
+                ServerLevel world = blockSource.level();
+                Direction direction = blockSource.state().getValue(DispenserBlock.FACING);
+                BlockPos pos = blockSource.pos();
+                switch (direction) {
+                    case DOWN -> pos = pos.below();
+                    case UP -> pos = pos.above();
+                    case EAST -> pos = pos.east();
+                    case WEST -> pos = pos.west();
+                    case NORTH -> pos = pos.north();
+                    case SOUTH -> pos = pos.south();
+                }
+                if(world.getBlockState(pos).getBlock() instanceof LavaCauldronBlock cauldron && cauldron.isFull(world.getBlockState(pos))) {
+                    world.setBlock(pos, Blocks.CAULDRON.defaultBlockState(), 3);
+                    ItemStack stack = new ItemStack(Items.LAVA_BUCKET);
+                    ItemEntity entityItem = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack.copy());
+                    if (stack.hasTag()) {
+                        entityItem.getItem().setTag(stack.getTag().copy());
+                    }
+                    world.addFreshEntity(entityItem);
+                    itemStack.split(1);
+                    return itemStack;
+                }
+                return super.execute(blockSource, itemStack);
+            }
+        });
+
+        LOGGER.info("COMMON SETUP END");
     }
 }
